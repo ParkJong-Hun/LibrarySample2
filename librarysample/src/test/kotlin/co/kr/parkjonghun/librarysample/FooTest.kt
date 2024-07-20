9945297cba0446b5cb6353b16fb92e53e6a50b42
Park JongHun <thesn515@gmail.com>
2024-07-20 22:06:48 +0900

    fix: test

--- librarysample/src/test/kotlin/co/kr/parkjonghun/librarysample/FooTest.kt
+++ librarysample/src/test/kotlin/co/kr/parkjonghun/librarysample/FooTest.kt
@@ -1,5 +1,6 @@
 package co.kr.parkjonghun.librarysample
 
+import Foo
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -8,6 +9,6 @@ class FooTest {
     @Test
     fun `test_foo`() {
         val actual = Foo().hoge(2.0)
-        assertEquals(4, 2 + 2)
+        assertEquals(6.28, actual)
     }
 }
